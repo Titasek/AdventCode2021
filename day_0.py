4b38c68215c76e4d7ab207cc119ce37f1b4187cd
Titasek <titasek.dev@gmail.com>
2021-12-02 06:42:24 +0100

    Template Script

---
+++ day_0.py
@@ -0,0 +1,15 @@
+"""
+    Template de base du script journalier.
+    Auteur : Titasek Dev
+    Date : 02 Dec. 2021
+"""
+
+with open('data') as input_datas:
+    datas = [line.strip() for line in input_datas.readlines()]
+
+answer = 0
+
+for data in datas:
+    pass
+
+print(answer)
